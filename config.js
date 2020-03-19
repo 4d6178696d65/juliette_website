539b193c12a2b3666a430d4f3916c688134de2c6
4d6178696d65 <62381795+4d6178696d65@users.noreply.github.com>
2020-03-19 12:43:48 +0100

    add metadata

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteTitle: 'Juliette website', // <title>
-  manifestName: 'SolidState',
+  manifestName: 'Educatrice canine',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/website-icon.png',
+  manifestIcon: 'src/assets/img/logo.png',
   pathPrefix: `/gatsby-starter-solidstate/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Juliette Chofflet',
   subHeading: 'Educatrice canine  ',
